dbb5574b76c0a19d7003d859722b5afd4407aabd
Christopher Kempton <ckempton@ip-10-1-101-124.ec2.internal>
2020-06-02 23:23:23 +0000

    save

--- lib/fusion_auth/groups.ex
+++ lib/fusion_auth/groups.ex
@@ -7,5 +7,73 @@ defmodule FusionAuth.Groups do
   ## Examples
   """
 
-  # @groups_url "/api/group"
+  @type group ::  %{
+    role_ids: [String.t()],
+    group: %{
+      data: map(),
+      name: String.t()
+    }
+  }
+
+  @type member ::  %{
+    user_id: String.t(),
+    data: map()
+  }
+
+  alias FusionAuth.Utils
+
+  @group_url "/api/group"
+  @member_url "#{@group_url}/member"
+
+  @spec create_group(FusionAuth.client(), map()) :: FusionAuth.request()
+  def create_group(client, group) do
+  end
+
+  @spec create_group(FusionAuth.client(), String.t(), map()) :: FusionAuth.request()
+  def create_group(client, group_id, group) do
+  end
+
+  @spec get_groups(FusionAuth.client()) :: FusionAuth.request()
+  def get_groups(client) do
+  end
+
+  @spec get_group(FusionAuth.client(), String.t()) :: FusionAuth.request()
+  def get_group(client, group_id) do
+  end
+
+  @spec update_group(FusionAuth.client(), String.t(), map()) :: FusionAuth.request()
+  def update_group(client, group_id, group) do
+  end
+
+  @spec delete_group(FusionAuth.client(), String.t()) :: FusionAuth.request()
+  def delete_group(client, group_id) do
+  end
+
+  @spec add_members(FusionAuth.client(), map()) :: FusionAuth.request()
+  def add_members(client, members) do
+  end
+
+  @spec add_members(FusionAuth.client(), String.t(), map()) :: FusionAuth.request()
+  def add_member(client, group_id, member) do
+  end
+
+  @spec add_members(FusionAuth.client(), list()) :: FusionAuth.request()
+  def remove_members(client, members) when is_list(members) do
+  end
+
+  @spec add_members(FusionAuth.client(), map()) :: FusionAuth.request()
+  def remove_members(client, members) when is_map(members) do
+  end
+
+  @spec add_members(FusionAuth.client(), String.t()) :: FusionAuth.request()
+  def remove_members(client, group_id) do
+  end
+
+  @spec add_members(FusionAuth.client(), String.t()) :: FusionAuth.request()
+  def remove_member(client, member_id) do
+  end
+
+  @spec add_members(FusionAuth.client(), String.t(), String.t()) :: FusionAuth.request()
+  def remove_member(client, group_id, user_id) do
+  end
 end
