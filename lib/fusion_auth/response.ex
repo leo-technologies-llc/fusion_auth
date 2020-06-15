ee1e4c2fc24b29350c15e36ffe62c4afc1950527
Christopher Kempton <ckempton@ip-10-1-101-124.ec2.internal>
2020-06-15 17:05:59 +0000

    add in response formatter

---
+++ lib/fusion_auth/response.ex
@@ -0,0 +1,43 @@
+defmodule FusionAuth.Response do
+  @moduledoc """
+  The `FusionAuth.Reports` module provides functions to format the response from FusionAuth.result.
+
+  ## Example
+
+  ```
+    iex> FusionAuth.client()
+    iex> |> FusionAuth.Groups.get_groups()
+    iex> |> FusionAuth.Response.format("groups", &atomize_keys/1)
+
+    {:ok, [%{id: "id", name: "name"}]
+  ```
+  """
+
+  @type result :: FusionAuth.result()
+
+  @spec format(result()) :: {:error, any()}
+  def format({:error, error, _}), do: {:error, error}
+
+  @spec format(result()) :: {:ok, map() | list()}
+  def format({:ok, data, _}), do: {:ok, data}
+
+  @spec format(result(), String.t()) :: {:error, any()}
+  def format({:error, error, _}, _), do: {:error, error}
+
+  @spec format(result(), String.t()) :: {:ok, map() | list()}
+  def format({:ok, data, _}, payload_key), do: {:ok, Map.get(data, payload_key)}
+
+  @spec format(result(), String.t(), fun()) :: {:error, any()}
+  def format({:error, error, _}, _, _), do: {:error, error}
+
+  @spec format(result(), String.t(), fun()) :: {:ok, map() | list()}
+  def format({:ok, data, _}, payload_key, formatter) do
+    payload = Map.get(data, payload_key, %{})
+    {:ok, process(payload, formatter)}
+  end
+
+  defp process(data, formatter) when is_list(data),
+    do: Enum.map(data, &process(&1, formatter))
+
+  defp process(data, formatter) when is_map(data), do: formatter.(data)
+end
---
+++ test/fusion_auth/response_test.exs
@@ -0,0 +1,46 @@
+defmodule FusionAuth.ResponseTest do
+  use ExUnit.Case
+
+  alias FusionAuth.Response
+
+  @error_payload {:error, "error", %Tesla.Env{}}
+  @success_payload {:ok, %{"groups" => [%{"id" => "id", "name" => "name"}]}, %Tesla.Env{}}
+
+  describe "format/1" do
+    test "when error" do
+      assert {:error, "error"} = Response.format(@error_payload)
+    end
+
+    test "when success" do
+      assert {:ok, %{"groups" => [%{"id" => "id", "name" => "name"}]}} =
+               Response.format(@success_payload)
+    end
+  end
+
+  describe "format/2" do
+    test "when error" do
+      assert {:error, "error"} = Response.format(@error_payload, "groups")
+    end
+
+    test "when success" do
+      assert {:ok, [%{"id" => "id", "name" => "name"}]} =
+               Response.format(@success_payload, "groups")
+    end
+  end
+
+  describe "format/3" do
+    test "when error" do
+      assert {:error, "error"} =
+               Response.format(@error_payload, "groups", fn record ->
+                 Map.put(record, "name", "processed")
+               end)
+    end
+
+    test "when success" do
+      assert {:ok, [%{"id" => "id", "name" => "processed"}]} =
+               Response.format(@success_payload, "groups", fn record ->
+                 Map.put(record, "name", "processed")
+               end)
+    end
+  end
+end
