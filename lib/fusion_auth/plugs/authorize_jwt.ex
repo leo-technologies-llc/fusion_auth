ba41cf927d47dd240cc1a9c02aff925904f73d5b
Brennan Karrer <brennan.karrer@leotechnologies.com>
2024-11-08 23:06:07 +0000

    chore: remove debug logs for verus_admin_server

--- lib/fusion_auth/plugs/authorize_jwt.ex
+++ lib/fusion_auth/plugs/authorize_jwt.ex
@@ -103,9 +103,7 @@ defmodule FusionAuth.Plugs.AuthorizeJWT do
         %{exp: claims["exp"], jti: claims["jti"]}
       )
     else
-      other ->
-        IO.inspect(other, label: "other throwing 401")
-
+      _ ->
         conn
         |> Plug.Conn.halt()
         |> Plug.Conn.send_resp(401, "Unauthorized")
@@ -130,10 +128,6 @@ defmodule FusionAuth.Plugs.AuthorizeJWT do
   defp verify_signature(token) do
     key = Application.get_env(:fusion_auth, :jwt_signing_key) |> Base.encode64()
     jwk = JWK.from(%{"kty" => "oct", "k" => key})
-
-    IO.inspect(token, label: "verify_signature token")
-    IO.inspect(key, label: "verify_signature key")
-    IO.inspect(jwk, label: "verify_signature jwk")
 
     case JWT.verify_strict(jwk, ["HS256"], token) do
       {false, _, _} ->
